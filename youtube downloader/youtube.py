f8f1841d5cf5359c3c5d111c714363aedf1af04d
Spyder <157384932+spydermask@users.noreply.github.com>
2025-03-20 14:38:36 +0100

    Update youtube.py
    Index

--- youtube downloader/youtube.py
+++ youtube downloader/youtube.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('font.html')
+    return render_template('index.html')
 
 @app.route('/download', methods=['POST'])
 def download():
